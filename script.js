b37c191c9341f6e2ae3b57f98feacd610bb7a263
Soo-Beom <soobeomkim77@gmail.com>
2021-02-14 17:48:33 -0800

    added drop down sections in index.html, style of dropdown to style.css, and onclick function for dropdown section to scipt.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function collapse() {
+    var collapse = document.getElementsByClassName("collapsible");
+    var i;
+    
+    for (i = 0; i < collapse.length; i++) {
+      collapse[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
+}
